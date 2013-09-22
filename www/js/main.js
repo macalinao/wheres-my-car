6f80fbf278d5b0ca7c9f87e6cde1df9a354aa345
Ian Macalinao <ianmacalinao@gmail.com>
2013-09-22 00:57:03 -0500

    Added geolocation crap
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- www/js/main.js
+++ www/js/main.js
@@ -1,90 +1,66 @@
-
-
-
-var canvas = oCanvas.create({ 
-    canvas: "#canvas", 
-    background: "#FFFFFF", 
-    fps: 60
-});
-
-var canvass = document.getElementById('canvas');
-if(canvass.getContext){
-	canvas.width = $(document).width();
-	canvas.height = $(document).height();
-}
-var arrow = canvas.display.polygon({
-	x: canvas.width/2,
-	y: canvas.height/2 - 100,
-	origin: {x: "center", y: "center"},
-	sides: 3,
-	radius: canvas.height/4,
-	rotation: 270,
-	fill: "#DEDEDE",
-	clickable_value: 0
-});
-var innerArrow = canvas.display.polygon({
-	x: arrow.radius/2,
-	y: 0,
-	origin: {x: 0, y:0},
-	sides: 3,
-	radius: arrow.radius/2,
-	rotation: 0,
-	fill: "#5CD65C",
-	zIndex: "front",
-	clickable_value: 0
-});
-$('#distance').css("top", canvas.height-arrow.x/5);
-
-
-canvas.addChild(arrow);
-arrow.addChild(innerArrow);
-
-function rotate(rot){
-	arrow.animate({
-		rotation: rot
-	}, {
-		duration:1000,
-		easing: "ease-in-out-cubic"
+document.addEventListener('deviceready', function() {
+	var canvas = oCanvas.create({ 
+	    canvas: "#canvas", 
+	    background: "#FFFFFF", 
+	    fps: 60
 	});
-	innerArrow.animate({
-		rotation: arrow.rotation - 270
-	}, {
-		duration:1000,
-		easing: "ease-in-out-cubic"
+
+	var eCanvas = document.getElementById('canvas');
+	if (eCanvas.getContext) {
+		canvas.width = $(document).width();
+		canvas.height = $(document).height();
+	}
+
+	var arrow = canvas.display.polygon({
+		x: canvas.width/2,
+		y: canvas.height/2 - 100,
+		origin: {x: "center", y: "center"},
+		sides: 3,
+		radius: canvas.height/4,
+		rotation: 270,
+		fill: "#DEDEDE",
+		clickable_value: 0
 	});
-}
+	var innerArrow = canvas.display.polygon({
+		x: arrow.radius/2,
+		y: 0,
+		origin: {x: 0, y:0},
+		sides: 3,
+		radius: arrow.radius/2,
+		rotation: 0,
+		fill: "#5CD65C",
+		zIndex: "front",
+		clickable_value: 0
+	});
+	$('#distance').css("top", canvas.height-arrow.x/5);
 
-function changeDistance(num){
-	$('#distance').text(num.toString() + " ft");
-}
 
-function onCompassError(error){
-	$('#distance').text("Cannot get location");
-}
-/*
-setInterval(function(){ 
-	navigator.compass.getCurrentHeading(function(){
-		rotate(heading);
-	}, onCompassError, compassOptions);
-}, 1000);
-*/
-function onSuccess(heading) {
-    alert('Heading: ' + heading.magneticHeading);
-};
+	canvas.addChild(arrow);
+	arrow.addChild(innerArrow);
 
-function onError(error) {
-    alert('CompassError: ' + error.code);
-};
+	function rotate(rot){
+		arrow.animate({
+			rotation: rot
+		}, {
+			duration:1000,
+			easing: "ease-in-out-cubic"
+		});
+		innerArrow.animate({
+			rotation: arrow.rotation - 270
+		}, {
+			duration:1000,
+			easing: "ease-in-out-cubic"
+		});
+	}
 
-navigator.compass.getCurrentHeading(onSuccess, onError);
-/*
-$('#distance').text(navigator.compass.getCurrentHeading(function(){
-		rotate(heading);
-	}, onCompassError, compassOptions);
-}, 1000););
-/*
-Use rotate(rot) and changeDistance(num) to manipulate rotation of arrow and remaining distance
-rot = value 0-360 to describe rotation (it starts at 270) to describe forward
-num = distance until destination
-
-*/
+	function updatePos() {
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			$('#distance').text(pos.coords.latitude + "\n" + pos.coords.longitude);
+			setTimeout(updatePos, 50);
+		}, function(err) {
+			$('#distance').text(err);
+			setTimeout(updatePos, 50);
+		});
+	}
+	updatePos();
+});
